e1e0d9a27b7a20b4f0b65dbbb88fff6deaaf69a9
hoken92 <hkenneth92@gmail.com>
2024-11-13 10:56:58 -0800

    movie search and random movie on load

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,5 +1,32 @@
-function Form() {
-  return <h1>Form</h1>;
+import { useState } from "react";
+
+function Form({ movieSearch }) {
+  const [formData, setFormData] = useState({
+    searchterm: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    movieSearch(formData.searchterm);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchterm"
+          onChange={handleChange}
+          value={formData.searchterm}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  );
 }
 
 export default Form;
--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,39 @@ import MovieDisplay from "../components/MovieDisplay";
 import Form from "../components/Form";
 
 function App() {
+  const apiKey = "e5de50b1";
+
+  const movieList = [
+    "Harry Potter",
+    "The Grinch",
+    "Mickey Mouse",
+    "Ghostbusters",
+    "Bruce Almighty",
+  ];
+  const randomNumber = Math.ceil(Math.random() * 5);
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovie(movieList[randomNumber - 1]);
+  }, []);
+
   return (
     <div className="App">
-      <Form />
-      <MovieDisplay />
+      <Form movieSearch={getMovie} />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
